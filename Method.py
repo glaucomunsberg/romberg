804e67b4da756faa1cfbd6a52ce6072e23582010
Deh.alba <andre90012002@gmail.com>
2014-07-13 15:39:34 -0300

    Funções modificadas

--- Method.py
+++ Method.py
@@ -3,10 +3,13 @@ from math 	import *
 from PrettyTable import *
 import sys
 sys.setrecursionlimit(10000)
+# a é igual a 0
 a = 0
-b = pi
+# b é igual a 20
+b = 20
 table = PrettyTable()
-iterations = range(9)
+# o numero de iterações que ela pede no problema é 8
+iterations = range(1,8)
 table.field_names = ['Interation','k','j','value']
 
 def romberg():
@@ -14,13 +17,25 @@ def romberg():
 	a=0
 	for k in iterations:
 		for j in iterations:
-			table.add_row([a,k,j, Rkj(k,j)])
+			# Colunas sem arredondamento
+			#table.add_row([a,k,j, Rkj(k,j)])
+			
+			# Colunas com arredontamento tipo 1
+			
+			table.add_row([a,k,j, round(Rkj(k,j),5) ])
+			
+			# Colunas com arredontamento tipo 2			
+			#table.add_row([a,k,j, '%.3f' %round(Rkj(k,j),5)])
+			
 			a+=1
 	print table
 
-def function(x):
+# Função base para resolver o problema.
+# -1.47206x10^-7x^10+0.0000148524x^9-0.000642464x^8+0.0155672x^7-0.231584x^6+2.17898x^5-12.861x^4+45.434x^3-85.9344x^2+65.5502x 
 
-	return sin(x) 
+def function(x):
+	
+	return -1.47206*(10**-7)*(x**10)+0.0000148524*(x**9)-0.000642464*(x**8)+0.0155672*(x**7)-0.231584*(x**6)+2.17898*(x**5)-12.861*(x**4)+45.434*(x**3)-85.9344*(x**2)+65.5502*(x)
 
 def h_of_k(k):
 
@@ -33,16 +48,20 @@ def R11():
 def Rk1(k,j):
 
 	if k == 1:
+		# Nunca vai cair nesse caso. O if do RKJ assegura isso.
 		return R11()
 	
 	else:
 		
-		somatorio = range( 1, 2**(k-2) )
-		totalSomatorio = 0
-
-		for i in somatorio:
-			total += function( a+(2*i-1) * h_of_k(k) )
-
+		#faz o somatorio de 1 até 2**(k-2) com a função abaixo.
+		#Não tem a necessidade de usar range.
+				
+		i=1
+		
+		for i in 2**(k-2):
+			
+			totalSomatorio += function( a+(2*i-1)*h_of_k(k) )
+		
 		return (1 / 2 ) * ( Rk1(k-1,1) + h_of_k(k-1) * totalSomatorio )
 
 def Rkj(k,j):
@@ -52,4 +71,4 @@ def Rkj(k,j):
 	elif j == 1:
 		return Rk1(k,j)
 	else:
-		return ( Rkj(k,j-1) + (Rkj(k,j-1) - Rkj(k-1,j-1) )* (4**(j-1)-1) ) 
+		return ( Rkj(k,j-1) + (((Rkj(k,j-1) - Rkj(k-1,j-1) )) / (4**(j-1)-1)) ) 
