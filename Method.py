f3202e4c82a377c28403bd1a1455b63443f39a79
Glauco Roberto Munsberg dos Santos <glaucomunsberg@gmail.com>
2014-07-15 18:57:26 -0300

    fixing the recursion call, menu and names

--- Method.py
+++ Method.py
@@ -1,8 +1,16 @@
 from numpy 	import *
 from math 	import *
 from PrettyTable import *
+from scipy import integrate
+from scipy.special import erf
 import sys
 sys.setrecursionlimit(10000)
+class bcolors:
+    HEADER = '\033[94m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
 # a e igual a 0
 a = 0
 # b e igual a 20
@@ -11,50 +19,54 @@ table = PrettyTable()
 # o numero de iteracoes que ela pede no problema e 8
 iterations = range(1,9)
 matrix = [[-1 for x in range(1,9)] for x in xrange(1,9)] 
-table.field_names = ['Interation','k','j','value']
+table.field_names = ['Interation','k','j','Resultado']
 
 def romberg():
 
-	i=0
+	it=0
 	for k in iterations:
-		for j in iterations:
+
+		interate = range(1,k+1)
+
+		for j in interate:
+
+			print "POS[",k,",",j,"]"
 			# Colunas sem arredondamento
 			#table.add_row([a,k,j, Rkj(k,j)])
 			
-			# Colunas com arredontamento tipo 1
+			# Colunas com arredontamento tipo 2
+			#print "result:",
+			table.add_row([it,k,j, '%.3f' %round(Result(k,j),5)])
+			it+=1
 			
-			table.add_row([i,k,j, round(Rkj(a,b),5) ])
-			
-			# Colunas com arredontamento tipo 2			
-			#table.add_row([a,k,j, '%.3f' %round(Rkj(k,j),5)])
-			
-			i+=1
 	print table
+	print matrix
 
 # Funcao base para resolver o problema.
 # -1.47206x10^-7x^10+0.0000148524x^9-0.000642464x^8+0.0155672x^7-0.231584x^6+2.17898x^5-12.861x^4+45.434x^3-85.9344x^2+65.5502x 
 
 def function(x):
-	
 	return -1.47206*(10**-7)*(x**10)+0.0000148524*(x**9)-0.000642464*(x**8)+0.0155672*(x**7)-0.231584*(x**6)+2.17898*(x**5)-12.861*(x**4)+45.434*(x**3)-85.9344*(x**2)+65.5502*(x)
 
 def h_of_k(k):
 
 	return (a-b)/2**(k-1)
 
-def R11(a,b):
-	print "RKJ [1,1]"
-	return ( (b-a) / 2 ) * ( function(a) + function(b) )
+def R11(k,j):
+	
+	matrix[k-1][j-1] = ( (b-a) / 2 ) * ( function(a) + function(b) )
+	return matrix[k-1][j-1]
 
 def Rk1(k,j):
 
-	print "RKJ [",k,",",j,"]"
+	
 	if matrix[k-1][j-1] != -1:
+		print "RK1[",k,",",j,"]",matrix[k-1][j-1]
 		return matrix[k-1][j-1]
 	else:
 		if k == 1:
 			# Nunca vai cair nesse caso. O if do RKJ assegura isso.
-			return R11()
+			return R11(k,j)
 		
 		else:
 			totalSomatorio = 0
@@ -66,15 +78,24 @@ def Rk1(k,j):
 			for i in rang:
 				totalSomatorio += function( a+(2*i-1)*h_of_k(k) )
 			
-			matrix[k-1][j-1] = (1 / 2 ) * ( Rk1(k-1,1) + h_of_k(k-1) * totalSomatorio )
+			matrix[k-1][j-1] = (1 / 2 ) * ( matrix[k-2][1] + h_of_k(k-1) * totalSomatorio )
 			return matrix[k-1][j-1]
 
 # Passar os indices e os valores.
 def Rkj(k,j):
-	print "RKJ [",k,",",j,"]"
+	matrix[k-1][j-1] = ( Result(k,j-1) + (((Result(k,j-1) - Result(k-1,j-1) )) / (4**(j-1)-1)) ) 
+	
+	return matrix[k-1][j-1]
+
+def Result(k,j):
 	if k == 1 and j == 1:
-		return R11()
-	elif j == 1:
+		return R11(k,j)
+	if j == 1:
 		return Rk1(k,j)
 	else:
-		return ( Rkj(k,j-1) + (((Rkj(k,j-1) - Rkj(k-1,j-1) )) / (4**(j-1)-1)) ) 
+		return Rkj(k,j)
+
+def ResultByScipy():
+	gaussian = lambda x: -1.47206*(10**-7)*(x**10)+0.0000148524*(x**9)-0.000642464*(x**8)+0.0155672*(x**7)-0.231584*(x**6)+2.17898*(x**5)-12.861*(x**4)+45.434*(x**3)-85.9344*(x**2)+65.5502*(x)
+	result = integrate.romberg(gaussian, 0, 1, show=True)
+	print("%g %g" % (2*result, erf(1)))
--- Principal.py
+++ Principal.py
@@ -6,33 +6,65 @@ import sys, time, datetime, os
 
 if __name__ == "__main__":
 
-	firstTime = False
-	os.system('clear')
-	
-	print '=====================Fermat====================='
-	print 'Option - Description'
-	print '     1 - Solve the problem using Romberg'
-	print '     2 - About'
-	print '     0 - Exit' 
-	print ''  
-	option = -7
-
-	while( option < 0  or option > 3):
-		try:
-			option = int(raw_input('Your Option: '))
-		except Exception,msg:
-			log.write('Principal: Error: '+str(msg))
-	if option == 1:    
-		romberg()
-	elif option == 0:
-		print 'Good Bye!'
-	elif option == 2:
-		print 'Biblioteca desenvolvida para solucionaro o problema'
-		print '     proposto no trabalho de Calculo Numerico. Para  saber'
-		print '     mais sobre o trabalho, por favor, acesse Ava/UFPel'
+	rangeOut = False
+	sair = False
+	while( not sair):
+		firstTime = False
+		os.system('clear')
+		print bcolors.HEADER + '============================= Romberg =========================' + bcolors.ENDC
+		print 'Option - Description'
+		print '     1 - Solve the problem using Romberg'
+		print '     2 - About'
+		print '     3 - Calculate f(x)'
+		print '     4 - Result with Scipy Library'
+		print '     0 - Exit' 
 		print ''
-		print 'Integrantes:'
-		print '		André Alba'
-		print '     Glauco Roberto'
-	else:
-		print 'Something is wrong!'
+		option = -7
+
+		while( option < 0  or option > 4):
+			try:
+				if rangeOut == True:
+					print bcolors.FAIL+'Out of range, try again' + bcolors.ENDC
+				option = int(raw_input('Option: '))
+				if option < 0  or option > 4:
+					rangeOut = True
+			except Exception,msg:
+				rangeOut = True
+
+		if option == 1:    
+			romberg()
+			raw_input('\nPress enter')
+		elif option == 0:
+			os.system('clear')
+			print bcolors.HEADER + '======================== Romberg - Exit ========================' + bcolors.ENDC
+			print 'Good Bye!'
+			sair = True
+		elif option == 2:
+			os.system('clear')
+			print bcolors.HEADER + '===================== Romberg - Integrantes =====================' + bcolors.ENDC
+			print 'Biblioteca desenvolvida para solucionaro o problema'
+			print '     proposto no trabalho de Calculo Numerico. Para  saber'
+			print '     mais sobre o trabalho, por favor, acesse Ava/UFPel'
+			print ''
+			print 'Integrantes:'
+			print '		André Alba'
+			print '     Glauco Roberto'
+			raw_input('\nPress enter')
+		elif option == 3:
+			os.system('clear')
+			print bcolors.HEADER + '===================== Romberg - Calculate ======================' + bcolors.ENDC
+			result = function(int(raw_input('Your \'x\' to function(x):')))
+			print 'Result to function(x)=',result
+			raw_input('Press enter')
+		elif option == 4:
+			os.system('clear')
+			print bcolors.HEADER + '====================== Romberg - Result =======================' + bcolors.ENDC
+			print 'Using Scipy Library'
+			print ''
+			result = ResultByScipy()
+			raw_input('\nPress enter')
+		else:
+			os.system('clear')
+			print bcolors.HEADER + '======================= Romberg - Ooops! ======================' + bcolors.ENDC
+			print bcolors.FAIL +'Something is wrong!' + bcolors.ENDC
+			sair = True
